14d048b038d37330a7480d0f4ae3408d33fdd9d3
Jon Kent <jonkent95@gmail.com>
2023-07-10 14:30:43 +0100

    recursion

--- app/interpolate.py
+++ app/interpolate.py
@@ -8,38 +8,23 @@ def interpolate_functions(template: Template, transforms: Transforms) -> ParseTr
 
     for k, v in template.items():
         if v.startswith(FUNCTION_PREFIX):
-            current_transform: Transform = transforms[v]
+            t: Transform = transforms[v]
 
-            if current_transform["post"]:
-                new_result = result.copy()
-                new_result["value"] = current_transform
-                current_transform: Transform = transforms[current_transform["post"]]
-
-                if current_transform["post"]:
-                    temp_value = current_transform
-                    temp_value["args"]["value"] = new_result["value"]
-                    result_value = temp_value
-                    current_transform: Transform = transforms[current_transform["post"]]
-
-                    if "post" in current_transform:
-                        temp_value = current_transform
-                        temp_value["args"]["value"] = new_result["value"]
-                        result_value = temp_value
-
-                    else:
-                        current_transform["args"]["value"] = temp_value
-                        temp_value = current_transform
-                        temp_value["args"]["value"] = result_value
-                        result_value = temp_value
-
-                    result[k] = result_value
-
-
-
-            print(result)
+            root = get_root(t, transforms)
+            result[k] = root
         else:
             result[k] = v
     return result
+
+
+def get_root(t: Transform, transforms: Transforms) -> Transform:
+    root = t
+    if "post" in t:
+        parent = transforms[t["post"]]
+        t.pop("post")
+        parent["args"]["value"] = t
+        root = get_root(parent, transforms)
+    return root
 
 
 def add_implicit_values(parse_tree: ParseTree) -> ParseTree:
