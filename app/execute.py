a7306abde7cd1cfbd03b001faf5cfd704b08341f
Jon Kent <jonkent95@gmail.com>
2023-07-11 17:59:54 +0100

    Fix function class

--- app/execute.py
+++ app/execute.py
@@ -1,53 +1,49 @@
 from copy import deepcopy
+from typing import Self
 
-from app.definitions import ParseTree, Transform
+from app.definitions import ParseTree, Transform, Value
 
 
 def execute(parse_tree: ParseTree) -> dict[str, str]:
     result: dict[str, str] = {}
     for k, v in parse_tree.items():
         if not isinstance(v, str):
-            result[k] = transform_to_function(v).apply()
+            result[k] = Function.from_transform(v).apply()
 
     return result
 
 
 class Function:
 
-    def __init__(self, value, args: dict[str, str]):
-        if isinstance(value, str):
+    _function_lookup: dict[str, Self.__class__]
+
+    def __init__(self, value: Value, args: dict[str, str]):
+        if value is None or isinstance(value, str):
             self._value = value
         else:
-            self._value = transform_to_function(value)
+            self._value = Function.from_transform(value)
         self._args = args
 
-    def perform(self, v: str, kwargs) -> str:
+    @classmethod
+    def from_transform(cls, transform: Transform) -> Self:
+        name = transform["name"]
+        args = deepcopy(transform["args"])
+        value = args.pop("value")
+        return cls._function_lookup.get(name)(value, args)
+
+    @classmethod
+    def set_function_lookup(cls, lookup: dict[str, Self.__class__]):
+        cls._function_lookup = lookup
+
+    def perform(self, value: str, **kwargs) -> str:
         pass
 
-    def apply(self) -> str:
+    def apply(self) -> str | None:
+        if self._value is None:
+            return None
         if not isinstance(self._value, str):
             v = self._value.apply()
         else:
             v = self._value
 
-        return self.perform(v, self._args)
-
-
-class Round(Function):
-
-    def perform(self, v: str, kwargs) -> str:
-        return round_up(v, **kwargs)
-
-
-def round_up(v: str, precision: str) -> str:
-    return "1"
-
-
-def transform_to_function(transform: Transform) -> Function:
-    name = transform["name"]
-    args = deepcopy(transform["args"])
-    value = args.pop("value")
-    if name == "ROUND":
-        return Round(value, args=args)
-
-
+        return self.perform(v, **self._args)
--- tests/unit/test_execute.py
+++ tests/unit/test_execute.py
@@ -1,25 +1,44 @@
 import unittest
 
 from app.definitions import ParseTree
-from app.execute import execute
+from app.execute import execute, Function
+
+
+class FakeRemoveChars(Function):
+
+    def perform(self, value: str, n: str = "1") -> str:
+        return value[int(n):]
+
+
+class FakeAdd(Function):
+
+    def perform(self, value: str, values: list[str] = []) -> str:
+        return str(sum([int(x) for x in (values + [value])]))
 
 
 class ExecutionTests(unittest.TestCase):
 
-    def test_execute(self):
+    def setUp(self) -> None:
+        function_lookup: dict[str, Function.__class__] = {
+            "REMOVE_CHARS": FakeRemoveChars,
+            "ADD": FakeAdd
+        }
+        Function.set_function_lookup(function_lookup)
+
+    def test_execute_single(self):
 
         parse_tree: ParseTree = {
             "150": {
-                "name": "ROUND",
+                "name": "REMOVE_CHARS",
                 "args": {
-                    "value": "1.2",
-                    "precision": "1",
+                    "value": "hello",
+                    "n": "2",
                 }
             }
         }
 
         expected = {
-            "150": "1",
+            "150": "llo",
         }
 
         actual = execute(parse_tree)
