efde47f2a9c15731532d98c595f401874968dbc7
Paul Lowther <paul.lowther01@hotmail.com>
2023-07-12 15:35:51 +0100

    Add AnyContains

--- app/functions.py
+++ app/functions.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 
 from app.execute import Function
 
@@ -14,6 +14,21 @@ class Contains(Function):
             return None
         if match_str in value:
             return on_true
+        return on_false
+
+
+class AnyContains(Function):
+    def perform(self,
+                value: Optional[str],
+                values: List[Optional[str]] = [],
+                match_str: str = "",
+                on_true: str = "1",
+                on_false: str = "2") -> Optional[str]:
+        all_values = [value] + values
+        for val in all_values:
+            if val is not None:
+                if match_str in val:
+                    return on_true
         return on_false
 
 
--- tests/unit/test_functions.py
+++ tests/unit/test_functions.py
@@ -1,5 +1,5 @@
 import unittest
-from app.functions import Contains, Exists
+from app.functions import Contains, Exists, AnyContains
 
 
 class ContainsTests(unittest.TestCase):
@@ -45,3 +45,40 @@ class ExistsTests(unittest.TestCase):
         expected = "2"
         self.assertEqual(expected, actual)
 
+
+class AnyContainsTests(unittest.TestCase):
+    def test_true(self):
+        value = "sentence for testing purposes"
+        values = []
+        args = {"values": values, "match_str": "testing", "on_true": "1", "on_false": "2"}
+        contains = AnyContains(value=value, args=args)
+        actual = contains.perform(value, **args)
+        expected = "1"
+        self.assertEqual(expected, actual)
+
+    def test_true_list(self):
+        value = "sometimes"
+        values = ["yes", "no"]
+        args = {"values": values, "match_str": "y", "on_true": "1", "on_false": "2"}
+        contains = AnyContains(value=value, args=args)
+        actual = contains.perform(value, **args)
+        expected = "1"
+        self.assertEqual(expected, actual)
+
+    def test_nones(self):
+        value = None
+        values = ["yes", "no", None]
+        args = {"values": values, "match_str": "y", "on_true": "1", "on_false": "2"}
+        contains = AnyContains(value=value, args=args)
+        actual = contains.perform(value, **args)
+        expected = "1"
+        self.assertEqual(expected, actual)
+
+    def test_all_nones(self):
+        value = None
+        values = [None, None, None]
+        args = {"values": values, "match_str": "y", "on_true": "1", "on_false": "2"}
+        contains = AnyContains(value=value, args=args)
+        actual = contains.perform(value, **args)
+        expected = "2"
+        self.assertEqual(expected, actual)
