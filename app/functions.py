ae27c8ca86a9ee33889fc67a0d56f944f0c948ea
Jon Kent <jonkent95@gmail.com>
2023-07-14 17:23:42 +0100

    Mathematical functions (#2)
    * mathematical functions
    
    ---------
    
    Co-authored-by: Paul Lowther <paul.lowther01@hotmail.com>

--- app/functions.py
+++ app/functions.py
@@ -1,7 +1,8 @@
+from decimal import Decimal, InvalidOperation, ROUND_HALF_UP
 from typing import List
 from datetime import datetime
 
-from app.definitions import Value
+from app.definitions import Value, Field
 
 
 def contains(
@@ -57,11 +58,64 @@ def _is_date(text: str) -> bool:
         return False
 
 
-def exists(
-        value: Value,
-        on_true: str = "1",
-        on_false: str = "2") -> Value:
-
+def exists(value: Value, on_true: str = "1", on_false: str = "2") -> Value:
     if value is not None:
         return on_true
     return on_false
+
+
+def round_half_up(value: Value, nearest: str = "1") -> Value:
+    v = _to_decimal(value, None)
+    if v is None:
+        return None
+
+    p = _to_decimal(nearest, Decimal("1"))
+    return str(Decimal(v / p).quantize(1, ROUND_HALF_UP) * p)
+
+
+def aggregate(value: Value, values: list[Value], weight: str) -> Value:
+    if _all_nones(value, values):
+        return None
+
+    return str(_to_decimal(value) + sum(_to_decimal(val) * _to_decimal(weight) for val in values))
+
+
+def mean(value: Value, values: list[Value]) -> Value:
+    if _all_nones(value, values):
+        return None
+
+    data = [_to_decimal(v) for v in [value] + values if v is not None]
+    divisor = len(data)
+    return str(sum(data) / divisor)
+
+
+def concat(value: Value, values: list[Value], seperator: str = " ") -> Value:
+    if _all_nones(value, values):
+        return None
+
+    return seperator.join([v for v in [value] + values if v is not None])
+
+
+def _to_decimal(value: Value, default: Decimal | None = Decimal(0)) -> Decimal:
+    if value is None:
+        return default
+    try:
+        return Decimal(value)
+    except InvalidOperation:
+        return default
+
+
+def _all_nones(*fields: Field) -> bool:
+    for f in fields:
+        if isinstance(f, dict):
+            for x in f.values():
+                if x is not None:
+                    return False
+        if isinstance(f, list):
+            for x in f:
+                if x is not None:
+                    return False
+        else:
+            if f is not None:
+                return False
+    return True
