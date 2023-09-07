210b99b8aed41f60a3158359ec8f82225fd39897
Jon Kent <jonkent95@gmail.com>
2023-09-07 10:57:22 +0100

    0002 working and fix abs

--- tests/integration/pck/test_abs.py
+++ tests/integration/pck/test_abs.py
@@ -10,9 +10,9 @@ class ABSPckTests(unittest.TestCase):
 
     def test_abs_to_pck(self):
         root_dir = "tests/data/abs/"
-        yaml_file_names = [f for f in os.listdir(root_dir) if f.endswith(".yaml")]
+        json_file_names = [f for f in os.listdir(root_dir) if f.endswith(".json")]
         print("------------")
-        for filename in yaml_file_names:
+        for filename in json_file_names:
 
             print("testing " + filename)
             filepath = root_dir + filename
