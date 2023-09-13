55b4a8e4e654b87f5f75b356836e1a0abb9c56b4
Angus Goody <angus.goody@gmail.com>
2023-09-13 16:43:31 +0100

    added last test

--- tests/integration/pck/test_qbs.py
+++ tests/integration/pck/test_qbs.py
@@ -49,5 +49,26 @@ class QBSTest(unittest.TestCase):
 
         self.assertTrue(are_equal(expected, actual))
 
+    def test_0001_zero_total_to_pck(self):
+        filepath = "tests/data/qbs/139.0001.zero.total.json"
+        submission_data = read_submission_data(filepath)
+
+        survey_metadata: SurveyMetadata = {
+            "survey_id": "139",
+            "period_id": "1604",
+            "ru_ref": "12345678901A",
+            "form_type": "0001",
+            "period_start_date": "2016-04-01",
+            "period_end_date": "2016-10-31",
+        }
+
+        actual: PCK = get_pck(submission_data, survey_metadata)
+
+        pck_filepath = "tests/data/qbs/139.0001.zero.total.pck"
+        with open(pck_filepath) as f:
+            expected: PCK = f.read()
+
+        self.assertTrue(are_equal(expected, actual))
+
 
 
