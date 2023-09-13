77c6513387de9199a33c2f5e4d45bebb2e0bc6cd
Jon Kent <jonkent95@gmail.com>
2023-09-13 17:01:18 +0100

    Onboard qpses, qpsespb, and qpsesrap

---
+++ tests/integration/pck/test_qpses.py
@@ -0,0 +1,29 @@
+import unittest
+
+from app.definitions import SurveyMetadata, PCK
+from app.pck import get_pck
+from tests.integration.pck import read_submission_data, are_equal
+
+
+class QPSESPckTests(unittest.TestCase):
+
+    def test_0002_to_pck(self):
+        filepath = "tests/data/qpses/160.0002.json"
+        submission_data = read_submission_data(filepath)
+
+        survey_metadata: SurveyMetadata = {
+            "survey_id": "160",
+            "period_id": "1605",
+            "ru_ref": "12346789012A",
+            "form_type": "0002",
+            "period_start_date": "2016-05-01",
+            "period_end_date": "20216-05-31",
+        }
+
+        actual: PCK = get_pck(submission_data, survey_metadata)
+
+        pck_filepath = "tests/data/qpses/160.0002.pck"
+        with open(pck_filepath) as f:
+            expected: PCK = f.read()
+
+        self.assertTrue(are_equal(expected, actual))
