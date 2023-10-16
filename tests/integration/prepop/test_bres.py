2a63db41f0f05335067bb2b28f47ba1af5304c28
Angus Goody <angus.goody@gmail.com>
2023-10-16 11:38:03 +0100

    Adding BRES test

---
+++ tests/integration/prepop/test_bres.py
@@ -0,0 +1,23 @@
+import json
+import unittest
+
+from app.definitions import PrepopData, Identifier, Template
+from app.pck_managers.prepop import get_prepop
+
+
+class BresTest(unittest.TestCase):
+
+    def test_bres(self):
+
+        survey_id = "221"
+        input_filepath = "tests/data/bres/prepop_bres_input.json"
+        with open(input_filepath) as f:
+            prepop_data: PrepopData = json.load(f)
+
+        output_filepath = "tests/data/tiles/prepop_output.json"
+        with open(output_filepath) as f:
+            expected: dict[Identifier: Template] = json.load(f)
+
+        actual = get_prepop(prepop_data, survey_id)
+
+        self.assertEqual(expected, actual)
