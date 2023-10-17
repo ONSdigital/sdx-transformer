e8d85375df982398f7407bc51ff1634010e3dff7
Angus Goody <angus.goody@gmail.com>
2023-10-17 09:50:56 +0100

    added structure to bres yaml

--- tests/integration/prepop/test_bres.py
+++ tests/integration/prepop/test_bres.py
@@ -14,10 +14,12 @@ class BresTest(unittest.TestCase):
         with open(input_filepath) as f:
             prepop_data: PrepopData = json.load(f)
 
-        output_filepath = "tests/data/tiles/prepop_output.json"
+        output_filepath = "tests/data/bres/prepop_bres_output.json"
         with open(output_filepath) as f:
             expected: dict[Identifier: Template] = json.load(f)
 
         actual = get_prepop(prepop_data, survey_id)
 
+        print("\n")
+        print(actual)
         self.assertEqual(expected, actual)
