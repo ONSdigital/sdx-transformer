cd491395110474471e3a380ae91136090de7a00d
Angus Goody <angus.goody@gmail.com>
2023-10-18 11:11:39 +0100

    tidied up

--- app/transform/clean.py
+++ app/transform/clean.py
@@ -9,18 +9,11 @@ BLACK_LIST: final = ["", Empty, [], {}]
 class CleanTreeWalker(TreeWalker):
 
     def on_dict(self, name: str, field: dict[str, Field]) -> Field:
-        d = super().on_dict(name, field)
-        return {k: v for k, v in d.items() if v not in BLACK_LIST}
+        return {k: v for k, v in super().on_dict(name, field).items() if v not in BLACK_LIST}
 
     def on_list(self, name: str, field: list[Field]) -> Field:
-        new_list = []
-        for item in field:
-            f = self.evaluate_field(name, item)
-            if f not in BLACK_LIST:
-                new_list.append(f)
-        return new_list
+        return [f for f in [self.evaluate_field(name, i) for i in field] if f not in BLACK_LIST]
 
 
 def clean(template: Template) -> Template:
-    tree: Template = CleanTreeWalker(template).walk_tree()
-    return {k: v for k, v in tree.items() if v not in BLACK_LIST}
+    return {k: v for k, v in CleanTreeWalker(template).walk_tree().items() if v not in BLACK_LIST}
