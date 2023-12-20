a0746446329f40805b2701781166db6e455226db
Angus Goody <angus.goody@gmail.com>
2023-12-20 16:38:28 +0000

    added another test for prepop

--- tests/integration/test_routes.py
+++ tests/integration/test_routes.py
@@ -17,13 +17,32 @@ class TestRoutes(unittest.TestCase):
         self.client = sdx_app.app.test_client()
 
     def test_process_prepop_with_invalid_json(self):
-        # Define the data to be sent in the request body
+
+        # Define some invalid data in the body
         data = None
 
         # Define query parameters
         query_params = {
             'survey_id': '123',
         }
+
+        # Send a POST request with JSON body and query parameters
+        response = self.client.post('/prepop',
+                                    data=json.dumps(data),
+                                    query_string=query_params,
+                                    content_type='application/json')
+
+        # Assert the HTTP status code (BAD request)
+        self.assertEqual(response.status_code, 400)
+
+    def test_process_prepop_without_a_survey_id(self):
+        # Define some valid json
+        data = {
+            "item1": "value1"
+        }
+
+        # Define empty query params
+        query_params = {}
 
         # Send a POST request with JSON body and query parameters
         response = self.client.post('/prepop',
