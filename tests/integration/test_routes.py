f362a40dc1425b3a7b1795bcf2facfd20d145609
Angus Goody <angus.goody@gmail.com>
2023-12-21 14:18:02 +0000

    made tests more specific

--- tests/integration/test_routes.py
+++ tests/integration/test_routes.py
@@ -1,8 +1,6 @@
 import json
 import unittest
 
-from sdx_gcp.errors import DataError, UnrecoverableError
-
 from app import sdx_app
 from app.routes import init_routes
 
@@ -35,6 +33,10 @@ class TestRoutes(unittest.TestCase):
         # Assert the HTTP status code (BAD request)
         self.assertEqual(response.status_code, 400)
 
+        # Assert the kind of error
+        response_json = response.json
+        self.assertEqual(response_json, {'Unrecoverable error': 'Data is not in json format'})
+
     def test_process_prepop_without_a_survey_id(self):
         # Define some valid json
         data = {
@@ -53,3 +55,34 @@ class TestRoutes(unittest.TestCase):
         # Assert the HTTP status code (BAD request)
         self.assertEqual(response.status_code, 400)
 
+        # Assert the kind of error
+        response_json = response.json
+        self.assertEqual(response_json, {'Unrecoverable error': 'Missing survey id from request'})
+
+    def test_process_pck_without_survey_id(self):
+        # Define some valid json
+        data = {
+            "item1": "value1"
+        }
+
+        # Define query params, without survey ID
+        query_params = {
+            "period_id": "123",
+            "ru_ref": "123",
+            "form_type": "123",
+            "period_start_date": "123",
+            "period_end_date":"123",
+        }
+
+        # Send a POST request with JSON body and query parameters
+        response = self.client.post('/pck',
+                                    data=json.dumps(data),
+                                    query_string=query_params,
+                                    content_type='application/json')
+
+        # Assert the HTTP status code (BAD request)
+        self.assertEqual(response.status_code, 400)
+
+        # Assert the kind of error
+        response_json = response.json
+        self.assertEqual(response_json, {'Unrecoverable error': 'Missing required parameter survey_id from request'})
