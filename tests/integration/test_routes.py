d6908cfd78f3a2fc9c9e5647292db86f044e3c2e
Angus Goody <angus.goody@gmail.com>
2023-12-21 15:59:23 +0000

    more tests

--- tests/integration/test_routes.py
+++ tests/integration/test_routes.py
@@ -7,12 +7,13 @@ from app.routes import init_routes
 
 class TestRoutes(unittest.TestCase):
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         # Set up the SDX app
         init_routes(sdx_app)
 
         # Create a test client for our SDX app
-        self.client = sdx_app.app.test_client()
+        cls.client = sdx_app.app.test_client()
 
     def test_process_prepop_with_invalid_json(self):
 
@@ -86,3 +87,58 @@ class TestRoutes(unittest.TestCase):
         # Assert the kind of error
         response_json = response.json
         self.assertEqual(response_json, {'Unrecoverable error': 'Missing required parameter survey_id from request'})
+
+    def test_process_pck_with_invalid_format_version_01(self):
+        # Define some valid json
+        data = None
+
+        # Define valid query params
+        query_params = {
+            "survey_id": "123",
+            "period_id": "123",
+            "ru_ref": "123",
+            "form_type": "123",
+            "period_start_date": "123",
+            "period_end_date": "123",
+        }
+
+        # Send a POST request with JSON body and query parameters
+        response = self.client.post('/pck',
+                                    data=json.dumps(data),
+                                    query_string=query_params,
+                                    content_type='application/json')
+
+        # Assert the HTTP status code (BAD request)
+        self.assertEqual(response.status_code, 400)
+
+        # Assert the kind of error
+        response_json = response.json
+        self.assertEqual(response_json, {'Unrecoverable error': 'Submission data is not in json format'})
+
+    def test_process_pck_with_invalid_format_version_03(self):
+        # Define some valid json
+        data = None
+
+        # Define valid query params
+        query_params = {
+            "survey_id": "123",
+            "period_id": "123",
+            "ru_ref": "123",
+            "form_type": "123",
+            "period_start_date": "123",
+            "period_end_date": "123",
+            "data_version": "0.0.3",
+        }
+
+        # Send a POST request with JSON body and query parameters
+        response = self.client.post('/pck',
+                                    data=json.dumps(data),
+                                    query_string=query_params,
+                                    content_type='application/json')
+
+        # Assert the HTTP status code (BAD request)
+        self.assertEqual(response.status_code, 400)
+
+        # Assert the kind of error
+        response_json = response.json
+        self.assertEqual(response_json, {'Unrecoverable error': 'Submission data is not in json format'})
