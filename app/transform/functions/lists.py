0d9a540feb5d834ed94248fdcac4d8e545a4f96e
Jon Kent <jonkent95@gmail.com>
2024-01-18 15:20:52 +0000

    Log image transformer param

--- app/transform/functions/lists.py
+++ app/transform/functions/lists.py
@@ -7,15 +7,17 @@ transformations within a build spec that result in a list.
 
 
 def as_list(_value: Value, values: list[Value]) -> list[Value]:
-    return values
+    return [v if v is not Empty else "" for v in values]
 
 
 def append_to_list(value: Value, values: list[Value]) -> list[Value]:
-    return values + [value]
+    full = values + [value]
+    return as_list("", values=full)
 
 
 def prepend_to_list(value: Value, values: list[Value]) -> list[Value]:
-    return [value] + values
+    full = [value] + values
+    return as_list("", values=full)
 
 
 def trim_list(_value: Value, values: list[Value]) -> list[Value]:
---
+++ tests/unit/transform/functions/test_list.py
@@ -0,0 +1,42 @@
+import unittest
+
+from app.definitions import Empty
+from app.transform.functions.lists import as_list, append_to_list, prepend_to_list, trim_list
+
+
+class ListTests(unittest.TestCase):
+
+    def test_as_list(self):
+        value = "foo"
+        values = ["a", Empty, "c"]
+        actual = as_list(value, values=values)
+        expected = ["a", "", "c"]
+        self.assertEqual(expected, actual)
+
+    def test_append_to_list(self):
+        value = "foo"
+        values = ["a", Empty, "c"]
+        actual = append_to_list(value, values=values)
+        expected = ["a", "", "c", "foo"]
+        self.assertEqual(expected, actual)
+
+    def test_prepend_to_list(self):
+        value = "foo"
+        values = ["a", Empty, "c"]
+        actual = prepend_to_list(value, values=values)
+        expected = ["foo", "a", "", "c"]
+        self.assertEqual(expected, actual)
+
+    def test_trim_list(self):
+        value = "foo"
+        values = ["a", Empty, "c"]
+        actual = trim_list(value, values=values)
+        expected = ["a", "c"]
+        self.assertEqual(expected, actual)
+
+    def test_trim_list_all_empty(self):
+        value = "foo"
+        values = [Empty, Empty, Empty]
+        actual = trim_list(value, values=values)
+        expected = []
+        self.assertEqual(expected, actual)
