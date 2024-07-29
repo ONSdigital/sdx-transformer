70a06b6a900edb930138600f23207a46c61c7656
ldearsley <ldearsley2@gmail.com>
2024-07-29 14:02:39 +0100

    Tests for BRES form types

--- tests/integration/looped/test_bres.py
+++ tests/integration/looped/test_bres.py
@@ -53,6 +53,84 @@ class BresTests(unittest.TestCase):
         print("")
         print(actual)
 
+    def test_0015_to_idbr_pck(self):
+
+        filepath = "tests/data/bres/221.0015.json"
+        submission_data = read_submission_data(filepath)
+
+        survey_metadata: SurveyMetadata = {
+            "survey_id": "221",
+            "period_id": "201605",
+            "ru_ref": "12345678901A",
+            "form_type": "0015",
+            "period_start_date": "2016-05-01",
+            "period_end_date": "2016-05-31",
+        }
+
+        actual: PCK = get_looping(submission_data, survey_metadata)
+
+        pck_filepath = "tests/data/bres/221.0015.pck"
+        with open(pck_filepath, 'rb') as f:
+            expected: PCK = f.read().decode()
+
+        # with open(pck_filepath, 'wb') as f:
+        #     f.write(actual.encode("utf-8"))
+
+        self.maxDiff = None
+        self.assertEqual(expected, actual)
+
+    def test_0016_to_idbr_pck(self):
+
+        filepath = "tests/data/bres/221.0016.json"
+        submission_data = read_submission_data(filepath)
+
+        survey_metadata: SurveyMetadata = {
+            "survey_id": "221",
+            "period_id": "201605",
+            "ru_ref": "12345678901A",
+            "form_type": "0016",
+            "period_start_date": "2016-05-01",
+            "period_end_date": "2016-05-31",
+        }
+
+        actual: PCK = get_looping(submission_data, survey_metadata)
+
+        pck_filepath = "tests/data/bres/221.0016.pck"
+        with open(pck_filepath, 'rb') as f:
+            expected: PCK = f.read().decode()
+
+        # with open(pck_filepath, 'wb') as f:
+        #     f.write(actual.encode("utf-8"))
+
+        self.maxDiff = None
+        self.assertEqual(expected, actual)
+
+    def test_0017_to_idbr_pck(self):
+
+        filepath = "tests/data/bres/221.0017.json"
+        submission_data = read_submission_data(filepath)
+
+        survey_metadata: SurveyMetadata = {
+            "survey_id": "221",
+            "period_id": "201605",
+            "ru_ref": "12345678901A",
+            "form_type": "0017",
+            "period_start_date": "2016-05-01",
+            "period_end_date": "2016-05-31",
+        }
+
+        actual: PCK = get_looping(submission_data, survey_metadata)
+
+        pck_filepath = "tests/data/bres/221.0017.pck"
+        with open(pck_filepath, 'rb') as f:
+            expected: PCK = f.read().decode()
+
+        # with open(pck_filepath, 'wb') as f:
+        #     f.write(actual.encode("utf-8"))
+
+        self.maxDiff = None
+        self.assertEqual(expected, actual)
+
     def test_0019_to_idbr_pck(self):
 
         filepath = "tests/data/bres/221.0019.json"
