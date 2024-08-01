0005dfd269dd7fce337b42a800bb1981163ce66b
Jon Kent <jonkent95@gmail.com>
2024-08-01 16:14:24 +0100

    fix test

--- tests/integration/looped/test_bres.py
+++ tests/integration/looped/test_bres.py
@@ -132,14 +132,14 @@ class BresTests(unittest.TestCase):
         actual: PCK = get_looping(submission_data, survey_metadata)
 
         pck_filepath = "tests/data/bres/221.0019.pck"
-        # with open(pck_filepath, 'rb') as f:
-        #     expected: PCK = f.read().decode()
+        with open(pck_filepath, 'rb') as f:
+            expected: PCK = f.read().decode()
 
-        with open(pck_filepath, 'wb') as f:
-            f.write(actual.encode("utf-8"))
+        # with open(pck_filepath, 'wb') as f:
+        #     f.write(actual.encode("utf-8"))
 
-        # self.maxDiff = None
-        # self.assertEqual(expected, actual)
+        self.maxDiff = None
+        self.assertEqual(expected, actual)
 
     def test_to_idbr_pck_new_lu(self):
 
