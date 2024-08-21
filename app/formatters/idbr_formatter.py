99cbc1ab2451914fad3e22ff92a5f39d0962df88
Jon Kent <jonkent95@gmail.com>
2024-08-21 17:40:33 +0100

    Create scan number from ruref, survey id and period

--- app/formatters/idbr_formatter.py
+++ app/formatters/idbr_formatter.py
@@ -1,5 +1,17 @@
+from typing import Optional
+
 from app.definitions import Value, SurveyMetadata, Empty
 from app.formatters.formatter import Formatter
+
+
+def _get_scan_number(metadata: SurveyMetadata, ref: Optional[str] = None) -> str:
+    """Create a scan number based on the passed reference.
+    If no reference is passed (as should be the case for the top level ru) then
+    create a unique number from the ruref, survey_id and period"""
+    if ref:
+        return f's{ref}'
+
+    return f's_{metadata["ru_ref"]}_{metadata["survey_id"]}_{metadata["period_id"]}'
 
 
 class IDBRFormatter(Formatter):
@@ -8,17 +20,17 @@ class IDBRFormatter(Formatter):
     Headers: ruref, checklet, luref, checklet, surveycode, period, formtype, pageno, scanno, batchno,
             qcode, qvalue
     """
-    def _pck_lines(self, data: dict[str, Value], metadata: SurveyMetadata, ref: str = "00000000") -> list[str]:
+    def _pck_lines(self, data: dict[str, Value], metadata: SurveyMetadata, ref: Optional[str] = None) -> list[str]:
         ru: str = metadata["ru_ref"]
         ru_ref: str = ru[0:-1] if ru[-1].isalpha() else ru
         checklet: str = ru[-1] if ru[-1].isalpha() else ""
         period: str = metadata["period_id"]
         survey_id = metadata["survey_id"]
         form_type = metadata["form_type"]
-        lu_ref = ref
+        lu_ref = ref if ref else "00000000"
         lu_checklet = "A"
         page_no = "001"
-        scan_no = f's{ref}'
+        scan_no = _get_scan_number(metadata, ref)
 
         line_list = []
         for qcode, value in sorted(data.items()):
