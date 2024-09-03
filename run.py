ca5c84456629c7447e38e95210beca19f284fbcd
Paul Lowther <paul.lowther@ons.gov.uk>
2024-09-03 15:15:57 +0100

    Upscale config content length

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
+from waitress import serve
+
 from app import sdx_app
 from app.routes import init_routes
 
 if __name__ == '__main__':
     init_routes(sdx_app)
-    sdx_app.run(port=5000)
+    def new_run(self, port: int = 5000):
+        """Start the server"""
+        print("hi")
+        serve(self.app, host='0.0.0.0', port=port, channel_timeout=300, inactivity=300)
+
+
+    sdx_app.run = new_run
+    sdx_app.run(sdx_app, port=5000)
