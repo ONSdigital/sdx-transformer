c8e2d0859712d852afb5346b6720788c13c078bd
ldearsley <ldearsley2@gmail.com>
2025-02-24 11:58:54 +0000

    Add prices schema

--- app/config/functions.py
+++ app/config/functions.py
@@ -6,7 +6,7 @@ from app.services.transform.functions.lists import as_list, append_to_list, prep
 from app.services.transform.functions.numerical import round_half_up, aggregate, mean, number_equals, total, divide
 from app.services.transform.functions.string import (starts_with, contains, any_contains,
                                                      concat, carve, string_padding,
-                                                     space_split, postcode_start, postcode_end)
+                                                     space_split, postcode_start, postcode_end, whitespace_removal)
 from app.services.transform.functions.time import (to_date, any_date, start_of_month,
                                                    end_of_month, start_of_year, end_of_year)
 
@@ -43,4 +43,5 @@ _function_lookup: dict[str, Callable] = {
     "SPACE_SPLIT": space_split,
     "POSTCODE_START": postcode_start,
     "POSTCODE_END": postcode_end,
+    "WHITESPACE_REMOVAL": whitespace_removal,
 }
---
+++ tests/integration/prepop/test_prices.py
@@ -0,0 +1,27 @@
+import json
+import unittest
+
+from app.controllers.prepop import get_prepop
+from app.definitions.input import Identifier, PrepopData
+from app.definitions.spec import Template
+
+
+class TestPrices(unittest.TestCase):
+
+    def test_prices(self):
+        self.maxDiff = None
+
+        survey_id = "132"
+        input_filepath = "tests/data/prices/prepop_prices_input.json"
+        with open(input_filepath) as f:
+            prepop_data: PrepopData = json.load(f)
+
+        output_filepath = "tests/data/prices/prepop_prices_output.json"
+        with open(output_filepath) as f:
+            expected: dict[Identifier: Template] = json.load(f)
+
+        actual = get_prepop(prepop_data, survey_id)
+
+        print(actual)
+
+        self.assertEqual(expected, actual)
