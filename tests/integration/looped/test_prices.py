5ec16e86294560ab42aabe122cd89a0b0dbec4b3
ldearsley <ldearsley2@gmail.com>
2025-04-07 11:48:23 +0100

    Test data

---
+++ tests/integration/looped/test_prices.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestPrices(unittest.TestCase):
+    def setup(self):
+        pass
+
+    def test_prices_0001(self):
+        """
+        Test prices 0001
+        """
+        self.assertTrue(True)
