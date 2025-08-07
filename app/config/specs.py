8aabca584e772c0ae59f383a367cd23fe78553a6
James Bryer <36200875+jamesbryer@users.noreply.github.com>
2025-08-07 16:54:27 +0100

    IPI upstream transformations (#90)
    * Working
    
    * Change country_of_origin to country_name for EPI
    
    * Change country_of_origin to country_name for EPI
    
    * Formatting
    
    * Bump version
    
    * Change output to country_of_origin with input as country_name

--- app/config/specs.py
+++ app/config/specs.py
@@ -55,6 +55,7 @@ _prepop_spec_mapping: dict[str, Selector[str, str]] = {
     "076": PrepopSelector("marine-prepop"),
     "132": PrepopSelector("prices-prepop"),
     "133": PrepopSelector("epi-prepop"),
+    "156": BuildSpecSelector("ipi-prepop"),
     "221": PrepopSelector("bres-prepop"),
     "241": PrepopSelector("brs-prepop"),
 }
---
+++ tests/integration/prepop/test_ipi.py
@@ -0,0 +1,57 @@
+import json
+import unittest
+
+from app.controllers.prepop import get_prepop
+from app.definitions.input import PrepopData, Identifier
+from app.definitions.spec import Template
+
+
+class TestIPI(unittest.TestCase):
+
+    def test_ipi(self):
+        self.maxDiff = None
+
+        survey_id = "156"
+        input_filepath = "tests/data/ipi/prepop_ipi_input.json"
+        with open(input_filepath) as f:
+            prepop_data: PrepopData = json.load(f)
+
+        output_filepath = "tests/data/ipi/prepop_ipi_output.json"
+        with open(output_filepath) as f:
+            expected: dict[Identifier: Template] = json.load(f)
+
+        actual = get_prepop(prepop_data, survey_id)
+
+        self.assertEqual(expected, actual)
+
+    def test_ipi_missing_value(self):
+        self.maxDiff = None
+
+        survey_id = "156"
+        input_filepath = "tests/data/ipi/prepop_ipi_missing_input.json"
+        with open(input_filepath) as f:
+            prepop_data: PrepopData = json.load(f)
+
+        output_filepath = "tests/data/ipi/prepop_ipi_missing_output.json"
+        with open(output_filepath) as f:
+            expected: dict[Identifier: Template] = json.load(f)
+
+        actual = get_prepop(prepop_data, survey_id)
+
+        self.assertEqual(expected, actual)
+
+    def test_ipi_multiple_items(self):
+        self.maxDiff = None
+
+        survey_id = "156"
+        input_filepath = "tests/data/ipi/prepop_ipi_multiple_input.json"
+        with open(input_filepath) as f:
+            prepop_data: PrepopData = json.load(f)
+
+        output_filepath = "tests/data/ipi/prepop_ipi_multiple_output.json"
+        with open(output_filepath) as f:
+            expected: dict[Identifier: Template] = json.load(f)
+
+        actual = get_prepop(prepop_data, survey_id)
+
+        self.assertEqual(expected, actual)
